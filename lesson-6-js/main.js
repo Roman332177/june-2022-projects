32bb73ae48eb0ee836d443b8638825b12137f5ad
Roman332177 <roman332177@gmail.com>
2022-07-27 15:25:25 +0300

    norm

---
+++ lesson-6-js/main.js
@@ -0,0 +1,122 @@
+
+let a1 = 1;
+let a2 = 2;
+let a3 = 3;
+let a4 = 4;
+let a5 = 5;
+let a6 = 6;
+let a7 = 7;
+let a8 = 8;
+let a9 = 9;
+let a10 = 10;
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers);
+
+let kobzarBook = {
+    title: 'Kobzar',
+    pageCount: 999,
+    genre: 'Poetry',
+    authors: [
+        {
+            name: 'Taras Taras Shevchenko',
+            age: 100,
+        },
+    ],
+};
+
+console.log(kobzarBook);
+
+let test1Book = {
+    title: 'Test Title',
+    pageCount: 77,
+    genre: 'Test genre',
+    authors: [
+        {
+            name: 'Test name 1',
+            age: 44,
+        },
+        {
+            name: 'Test name 2',
+            age: 55,
+        },
+    ],
+};
+console.log(test1Book);
+
+const test2Book = {
+    title: 'Test Title',
+    pageCount: 356,
+    genre: 'Test genre',
+    authors: [
+        {
+            name: 'Test name 1',
+            age: 88,
+        },
+        {
+            name: 'Test name 2',
+            age: 77,
+        },
+        {
+            name: 'Test name 3',
+            age: 66,
+        },
+    ],
+};
+
+console.log(test2Book);
+
+let users = [
+    {
+        name: 'Taras',
+        username: 'drako',
+        password: 'lalala',
+    },
+    {
+        name: 'Roman',
+        username: 'romashka',
+        password: 'bebebeb',
+    },
+    {
+        name: 'Test name 1',
+        username: 'Test username 1',
+        password: 'Test password 1',
+    },
+    {
+        name: 'Test name 2',
+        username: 'Test username 2',
+        password: 'Test password 2',
+    },
+    {
+        name: 'Test name 3',
+        username: 'Test username 3',
+        password: 'Test password 3',
+    },
+    {
+        name: 'Test name 4',
+        username: 'Test username 4',
+        password: 'Test password 4',
+    },
+    {
+        name: 'Test name 5',
+        username: 'Test username 5',
+        password: 'Test password 5',
+    },
+    {
+        name: 'Test name 6',
+        username: 'Test username 6',
+        password: 'Test password 6',
+    },
+    {
+        name: 'Test name 7',
+        username: 'Test username 7',
+        password: 'Test password 7',
+    },
+    {
+        name: 'Test name 8',
+        username: 'Test username 8',
+        password: 'Test password 8',
+    },
+];
+
+console.log(users);
